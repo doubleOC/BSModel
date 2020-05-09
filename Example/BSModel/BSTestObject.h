9f3e312da46f36b04e9935656a985b0f62aff322
future <>
2020-05-09 15:16:44 +0800

    wcc

---
+++ Example/BSModel/BSTestObject.h
@@ -0,0 +1,17 @@
+//
+//  BSTestObject.h
+//  BSModel_Example
+//
+//  Created by future on 2020/5/9.
+//  Copyright © 2020 3923240@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BSTestObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/BSModel/BSTestObject.m
@@ -0,0 +1,13 @@
+//
+//  BSTestObject.m
+//  BSModel_Example
+//
+//  Created by future on 2020/5/9.
+//  Copyright © 2020 3923240@qq.com. All rights reserved.
+//
+
+#import "BSTestObject.h"
+
+@implementation BSTestObject
+
+@end
